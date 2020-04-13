075752b8dc637387e12057d13891761ee02b7b17
Dylan <dxs5670@psu.edu>
2020-04-13 09:19:16 -0400

    Updated w/ Node, Express, Bootstrap & JQuery
    Navigation across all pages, stylesheets will need updated

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+// initializing express
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'),{extensions:'html'}));
+
+
+// express sets up the home route
+app.use('/', indexRouter);
+app.use('/users', usersRouter); // just an example
+
+module.exports = app;
