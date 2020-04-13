e9c79aa66f4bf8493c8c5e60f22a90844caf7b8f
aprincivalle <58524307+aprincivalle@users.noreply.github.com>
2020-04-13 16:18:33 -0400

    updated scripts for clubs and orgs

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,21 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+/* Clubs & Orgs */ 
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
